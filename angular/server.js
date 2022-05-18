61af8814c486601112767668601ef87676faf18f
metin-gercek <metin.gercek@edu.bc.fi>
2022-05-18 11:00:33 +0300

    first commit

--- angular/server.js
+++ angular/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/aroundhelsinki-main'));
+app.use(express.static('./dist/aroundhelsinki'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/aroundhelsinki-main/'}),
+    res.sendFile('index.html', {root: 'dist/aroundhelsinki/'}),
 );
 
 // Start the app by listening on the default Heroku port
